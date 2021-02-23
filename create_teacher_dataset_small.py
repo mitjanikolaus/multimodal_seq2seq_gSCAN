cd9eb97821fc13ee3dbd5660345d08e2fe2a76da
Mitja Nikolaus <mitja.nikolaus@posteo.de>
2021-02-23 15:34:27 +0100

    Update sbatch scripts

--- create_teacher_dataset_small.py
+++ create_teacher_dataset_small.py
@@ -4,11 +4,11 @@ import random
 data = json.load(open("data/teacher/dataset.txt"))
 print(len(data["examples"]["train"]))
 
-data["examples"]["train"] = random.sample(data["examples"]["train"], 154000)
+data["examples"]["train"] = random.sample(data["examples"]["train"], 386000)
 
 print(len(data["examples"]["train"]))
 
-with open("data/teacher_small_20/dataset.txt", 'w') as outfile:
+with open("data/teacher_small_50/dataset.txt", 'w') as outfile:
     json.dump(data, outfile)
 
 print("success")
--- sbatch-scripts/train_teacher.sh
+++ sbatch-scripts/train_teacher.sh
@@ -7,11 +7,11 @@
 #SBATCH --mem=64000
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:1
-#SBATCH --output=out/train.out
-#SBATCH --error=out/train.out
+#SBATCH --output=out/train_50.out
+#SBATCH --error=out/train_50.out
 
 source activate gscan
 
 # train model on teacher train split
-python -u -m seq2seq --mode=train --max_decoding_steps=120 --max_testing_examples=2000 --data_directory=data/teacher_small_20 --attention_type=bahdanau --no_auxiliary_task --conditional_attention --output_directory=out_teacher_small_20 --training_batch_size=200 --max_training_iterations=200000 --seed=1
+python -u -m seq2seq --mode=train --max_decoding_steps=120 --max_testing_examples=2000 --data_directory=data/teacher_small_50 --attention_type=bahdanau --no_auxiliary_task --conditional_attention --output_directory=out_teacher_small_50 --training_batch_size=200 --max_training_iterations=200000 --seed=1
 
