8908a8817daa40cecda33d61969bb95975c67253
Mitja Nikolaus <mitja.nikolaus@posteo.de>
2021-03-08 09:09:41 +0100

    Add sbatch scripts

---
+++ sbatch-scripts/test_teacher.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+#SBATCH --job-name=gscan
+#
+#SBATCH --ntasks=1
+#SBATCH --time=12:00:00
+#SBATCH --mem=32000
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:1
+#SBATCH --output=out/test.out
+#SBATCH --error=out/test.out
+
+source activate gscan
+
+
+# test teacher model on test splits
+#python -u -m seq2seq --mode=test --data_directory=data/compositional_splits --attention_type=bahdanau --no_auxiliary_task --conditional_attention --output_directory=out_teacher --resume_from_file=out_teacher/model_best.pth.tar --splits=test,visual,visual_easier,situational_1,situational_2,contextual,adverb_1,adverb_2 --output_file_name=teacher.json --max_decoding_steps=120 &> out_teacher/test.txt
+
+# test small teacher model with LM
+python -u -m seq2seq --mode=test --data_directory=data/compositional_splits --attention_type=bahdanau --no_auxiliary_task --conditional_attention --output_directory=out_teacher_lm_small_50 --resume_from_file=out_teacher_lm_small_50/model_best.pth.tar --splits=test,visual,visual_easier,situational_1,situational_2,contextual,adverb_1,adverb_2 --output_file_name=teacher_lm_small_50.json --max_decoding_steps=120 &> out_teacher_lm_small_50/test.txt
+
+
---
+++ sbatch-scripts/train_teacher.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+#SBATCH --job-name=gscan
+#
+#SBATCH --ntasks=1
+#SBATCH --time=48:00:00
+#SBATCH --mem=64000
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:1
+#SBATCH --output=out/train_teacher_lm_50.out
+#SBATCH --error=out/train_teacher_lm_50.out
+
+source activate gscan
+
+# train model on teacher train split
+python -u -m seq2seq --mode=train --max_decoding_steps=120 --max_testing_examples=2000 --data_directory=data/teacher_small_50 --attention_type=bahdanau --no_auxiliary_task --conditional_attention --output_directory=out_teacher_lm_small_50 --training_batch_size=200 --max_training_iterations=200000 --seed=1
+
