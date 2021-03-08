47c02cf106cef12069ccfa2fcdc9b6f366c7d1e5
Mitja Nikolaus <mitja.nikolaus@posteo.de>
2021-03-08 16:01:40 +0100

    Revert "Fix imports"
    This reverts commit 9fda39574f33424208cb4858943f2d29588b5d1b.

--- seq2seq/train_teacher_to_learner.py
+++ seq2seq/train_teacher_to_learner.py
@@ -1,10 +1,15 @@
 import logging
 import torch
+import os
 
+from torch.nn.utils.rnn import pad_sequence
 from torch.optim.lr_scheduler import LambdaLR
 
-from helpers import log_parameters
-from evaluate import evaluate
+from seq2seq.model import Model
+from seq2seq.gSCAN_dataset import GroundedScanDataset
+from seq2seq.helpers import log_parameters
+from seq2seq.evaluate import evaluate
+
 
 
 logger = logging.getLogger(__name__)
