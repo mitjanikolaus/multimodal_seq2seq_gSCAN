d1ac9998ca3a98a4d40b0884cb88960541bf490f
Mitja Nikolaus <mitja.nikolaus@posteo.de>
2021-04-13 17:35:32 +0200

    Update plotting script

--- evaluation/plot_accuracies.py
+++ evaluation/plot_accuracies.py
@@ -8,16 +8,34 @@ import pandas as pd
 import seaborn as sns
 
 
-DEFAULT_DIR = os.path.expanduser("~/lis-cluster/multimodal_seq2seq_gSCAN/out_learner_t_l_eval_start_lm")
-
 BATCH_SIZE = 200
 START_ITERATION = 192053
 
 
-def plot(args):
+def plot_train_losses(args):
+    train_logs = json.load(open(os.path.join(args.data_dir, "train_logs.json")))
+    eval_logs = json.load(open(os.path.join(args.data_dir, "eval_logs.json")))
+
+    train_logs = pd.DataFrame(train_logs).T
+    eval_logs = pd.DataFrame(eval_logs).T
+
+    train_logs["samples"] = train_logs.index.map(lambda x: (int(x) * BATCH_SIZE) - (START_ITERATION * BATCH_SIZE))
+    eval_logs["samples"] = eval_logs.index.map(lambda x: (int(x) * BATCH_SIZE) - (START_ITERATION * BATCH_SIZE))
+
+    train_logs.set_index("samples", inplace=True)
+    eval_logs.set_index("samples", inplace=True)
+
+    # sns.lineplot(data=eval_logs[["exact_match"]])
+    # plt.show()
+
+    sns.lineplot(data=train_logs[["loss", "actions_loss", "lm_loss"]])
+    plt.show()
+
+
+def plot_eval_accuracies(args):
     accs = []
     baseline = None
-    pathlist = Path(args.data_dir).rglob('*.json')
+    pathlist = Path(args.data_dir).rglob('*_accuracies.json')
     for path in pathlist:
         path_in_str = str(path)
         with open(path) as f:
@@ -50,7 +68,8 @@ def plot(args):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--data-dir", type=str, default=DEFAULT_DIR, help="Dir with accuracy JSON files")
+    parser.add_argument("--data-dir", type=str, required=True, help="Dir with accuracy JSON files")
     args = parser.parse_args()
 
-    plot(args)
+    plot_train_losses(args)
+    plot_eval_accuracies(args)
